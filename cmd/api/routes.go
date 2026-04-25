6669757b3a4db5c23a5c05be04c0f8ed7d64a850
James LeVasseur <jlevdev@gmail.com>
2026-04-24 23:32:00 -0400

    switched to graphql only

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -23,30 +23,6 @@ func (app *application) routes() http.Handler {
 	mux.Group(func(r chi.Router) {
 		r.Use(app.requireAuth)
 
-		r.Route("/users", func(r chi.Router) {
-			r.Get("/", app.ListUsers)
-			r.Post("/", app.CreateUser)
-			r.Get("/{id}", app.GetUser)
-			r.Put("/{id}", app.UpdateUser)
-			r.Delete("/{id}", app.DeleteUser)
-		})
-
-		r.Route("/worlds", func(r chi.Router) {
-			r.Get("/", app.ListWorlds)
-			r.Post("/", app.CreateWorld)
-			r.Get("/{id}", app.GetWorld)
-			r.Put("/{id}", app.UpdateWorld)
-			r.Delete("/{id}", app.DeleteWorld)
-		})
-
-		r.Route("/map-assets", func(r chi.Router) {
-			r.Get("/", app.ListMapAssets)
-			r.Post("/", app.CreateMapAsset)
-			r.Get("/{id}", app.GetMapAsset)
-			r.Put("/{id}", app.UpdateMapAsset)
-			r.Delete("/{id}", app.DeleteMapAsset)
-		})
-
 		gqlSrv := handler.New(graph.NewExecutableSchema(graph.Config{
 			Resolvers: &graph.Resolver{DB: app.DB},
 		}))
